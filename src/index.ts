65178939b4c197a0ae6d9c66ca69580c6ca4ffbf
jordanbeamsley <jordanbeamsley@gmail.com>
2023-03-11 20:35:18 +0800

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { add } from "./utils";
+
+function init() {
+  const form = document.querySelector("form");
+  form?.addEventListener("submit", submitHandler);
+}
+
+function submitHandler(e: Event) {
+  e.preventDefault();
+  const a = document.querySelector("input[name='a']") as HTMLInputElement;
+  const b = document.querySelector("input[name='b']") as HTMLInputElement;
+  const result = add(Number(a.value), Number(b.value));
+  const resultElement = document.querySelector("p");
+  if (resultElement) {
+    resultElement.textContent = result.toString();
+  }
+}
+
+init();
